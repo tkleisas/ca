6b0f425d42ca09b3a4b32d545b32e510cad7f503
tasos.kleisas <tasos.kleisas@gmail.com>
2026-05-07 23:51:57 +0300

    TUI: byte-level escape parsing (fix [B leak), braille spinner on status line

--- ca_tui.ts
+++ ca_tui.ts
@@ -263,6 +263,7 @@ export class Tui {
   }
 
   shutdown() {
+    if (this.spinnerInterval) { clearInterval(this.spinnerInterval); this.spinnerInterval = null; }
     Deno.stdin.setRaw(false);
     Deno.stdout.writeSync(this.encoder.encode(
       esc("?25h") + esc("?1049l")
